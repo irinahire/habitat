d7486aa18dbedc827afe2fffcf92126c7175a1b5
BlueLab <irinahire.ia@gmail.com>
2026-07-16 19:47:00 -0300

    Update page.tsx

--- apps/irina/app/home/page.tsx
+++ apps/irina/app/home/page.tsx
@@ -2,14 +2,20 @@ import { PageBody, PageHeader } from '@kit/ui/page';
 
 import { DashboardDemo } from '~/home/_components/dashboard-demo';
 
+import { leerRegistro, escribirRegistro } from './actions'
+
 export default function HomePage() {
   return (
-    <>
-      <PageHeader description={'Your SaaS at a glance'} />
+    <div>
+      <button onClick={async () => {
+        const res = await leerRegistro('50964620-8c65-496d-97ed-3913896316ff')
+        console.log("Leído:", res)
+      }}>LEER REGISTRO</button>
 
-      <PageBody>
-        <DashboardDemo />
-      </PageBody>
-    </>
-  );
+      <button onClick={async () => {
+        const res = await escribirRegistro('50964620-8c65-496d-97ed-3913896316ff', { test: "dato nuevo" })
+        console.log("Escrito:", res)
+      }}>ESCRIBIR REGISTRO</button>
+    </div>
+  )
 }
