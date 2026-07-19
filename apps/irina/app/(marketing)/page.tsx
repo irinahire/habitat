46c3c0396d70aa79f68acce5bbe5eedaf23b9891
BlueLab <irinahire.ia@gmail.com>
2026-07-19 20:52:35 -0300

    Update page.tsx

--- apps/irina/app/(marketing)/page.tsx
+++ apps/irina/app/(marketing)/page.tsx
@@ -28,7 +28,7 @@ function Home() {
           }
           title={
             <>
-              <span>The ultimate SaaS Starter</span>
+              <span>The SaaS Starter</span>
               <span>for your next project</span>
             </>
           }
